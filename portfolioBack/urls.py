4322c5ab528d8370ad3ba24624668c2ce9e0a00f
Omar Atef <20190356@stud.fci-cu.edu.eg>
2022-04-24 15:09:26 +0200

    fix

--- portfolioBack/urls.py
+++ portfolioBack/urls.py
@@ -15,12 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
 from django.urls import path, include
 from portfilio import views as port
 
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('portfilio.urls')),
+    path('', include('portfilio.urls')),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 
 ]

89a4372a394f03468d8f413a1e12ece2f305eb3b
Omar Atef <20190356@stud.fci-cu.edu.eg>
2022-05-12 17:59:54 +0200

    V2.0(FIX)

--- portfilio/models.py
+++ portfilio/models.py
@@ -9,7 +9,7 @@ class Message(models.Model):
     Email = models.CharField(max_length=500)
     Subject = models.CharField(max_length=500)
     Message = models.TextField()
-    id = models.DateTimeField(primary_key=True, auto_now_add=True)
+    id = models.IntegerField(primary_key=True, editable=False, unique=True)
 
     def __str__(self):
         return str(self.Username) + " Sent " + str(self.Subject)
